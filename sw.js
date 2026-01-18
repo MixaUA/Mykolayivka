e6eddf86f9ffdfe1d44c65f58353a7f00633199c
MixaUA <121048487+MixaUA@users.noreply.github.com>
2026-01-18 23:20:25 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mykolaivka-new-cache-v23'; // Оновлено версію
+const CACHE_NAME = 'mykolaivka-new-cache-v24'; // Оновлено версію
 const API_CACHE_NAME = 'mykolaivka-new-api-cache-v1';
 
 const urlsToCache = [
